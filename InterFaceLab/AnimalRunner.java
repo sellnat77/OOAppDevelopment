e934b700fe09ef3c97627b461bba7173b51878eb
Russell <sell_nat@yahoo.com>
2014-09-09 13:51:15 -0700

    Removed comments after merge

--- InterFaceLab/AnimalRunner.java
+++ InterFaceLab/AnimalRunner.java
@@ -1,15 +1,3 @@
-/*import java.util.*;
-
-public class AnimalRunner
-{
-   public static void main(String[] args)
-   {
-      ArrayList<Dog> dogcatList = new ArrayList<Dog>();
-      dogcatList.add(new Dog("Fred"));
-      dogcatList.add(new Cat("Wanda"));
-   }
-}
-*/
 import java.util.*;
 
 public class AnimalRunner
