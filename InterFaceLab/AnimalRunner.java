987541958675bcc2b24af29f39264273f3470f68
Russell <sell_nat@yahoo.com>
2014-09-09 13:39:43 -0700

    formatted to print

--- InterFaceLab/AnimalRunner.java
+++ InterFaceLab/AnimalRunner.java
@@ -17,8 +17,10 @@ public class AnimalRunner
    public static void main(String[] args)
    {
       ArrayList<speakable> dogcatList = new ArrayList<speakable>();
+
       dogcatList.add(new Dog("Fred"));
       dogcatList.add(new Cat("Wanda"));
+
       for (speakable talk : dogcatList)
       {
          talk.speak();
