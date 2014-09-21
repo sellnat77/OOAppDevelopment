f5460b3402ee1242eedaaaf2a081797847d4c62f
sellnat77 <sell_nat@yahoo.com>
2014-09-21 00:06:12 -0700

    Done with project, needs documentation

--- MemoIO/Reader/memoReader.java
+++ MemoIO/Reader/memoReader.java
@@ -19,66 +19,66 @@ public class memoReader
 		int k;
 		String buffer = "";
 		int more = 1;
+
+		JOptionPane.showMessageDialog(null,"Please select your memo file");
 		try
 		{
-
-			JFileChooser chooser = new JFileChooser();
+		    JFileChooser chooser = new JFileChooser();
 			if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
 			{
-			   File selectedFile = chooser.getSelectedFile();
-			   Scanner in = new Scanner(selectedFile);
+			    File selectedFile = chooser.getSelectedFile();
+			    Scanner in = new Scanner(selectedFile);
 
-			   while(more == 1)
-			   {
-				   for(k = 0; k < 3; k++)
-			   	   {
-					   if(in.hasNextLine())
-					   {
-						   switch(k)
-						   {
-						   case 0:
-						         buffer += "\nSUBJECT:    " + in.nextLine();
-						         break;
-						   case 1:
-						   		 buffer += "\nDate Written:    " + in.nextLine();
-						   		 break;
-						   case 2:
-						   		 buffer += "\nMEMO:    " + in.nextLine();
-						   		 break;
-						   default:
-						   		 break;
+			    while(more == 1)
+			    {
+			 	    for(k = 0; k < 3; k++)
+			    	{
+			 		    if(in.hasNextLine())
+			 		    {
+			 			    switch(k)
+			 			    {
+			 			        case 0: buffer += "\nSUBJECT:    " + in.nextLine();
+			 			    	        break;
+
+						    	case 1: buffer += "\nDate Written:    " + in.nextLine();
+						    			break;
+
+						    	case 2: buffer += "\nMEMO:    " + in.nextLine();
+						    			break;
+
+						    	default:break;
 						    }
-					   }
-					   else
-					   {
-						   System.out.println("You are out of memos!");
-						   buffer = "No more memos!";
-						   more = 2;
-						   break;
-					   }
-				   }
-				   if(more == 1)
-				   {
-					   JOptionPane.showMessageDialog(null,buffer);
-					   buffer = "";
-					   System.out.println("\n\n\nWould you like to see the next memo?");
-					   System.out.println("\t1)Yes please!");
-					   System.out.println("\t2)No thats okay.");
-					   try
-					   {
-							more = userInput.nextInt();
-					   }
-					   catch(InputMismatchException e)
-					   {
-						   System.out.println("Enter either 1 or 2!");
-						   userInput.next();
-						   more = userInput.nextInt();
-					   }
-				   }
-				   else
-				   {
-					   break;
-				   }
+					    }
+					    else
+					    {
+						    System.out.println("You are out of memos!");
+						    JOptionPane.showMessageDialog(null,"No more memos!");
+						    more = 2;
+						    break;
+					    }
+				    }
+				    if(more == 1)
+				    {
+					    JOptionPane.showMessageDialog(null,buffer);
+				        buffer = "";
+					    System.out.println("\n\n\nWould you like to see the next memo?");
+					    System.out.println("\t1)Yes please!");
+					    System.out.println("\t2)No thats okay.");
+					    try
+					    {
+						    more = userInput.nextInt();
+					    }
+					    catch(InputMismatchException e)
+					    {
+						    System.out.println("Enter either 1 or 2!");
+						    userInput.next();
+						    more = userInput.nextInt();
+					    }
+				    }
+				    else
+				    {
+				 	    break;
+				    }
 			   }
 			}
 		}
@@ -86,16 +86,5 @@ public class memoReader
 		{
 			error.printStackTrace();
 		}
-	}
-
-	void readMultipleMemos()
-	{
-		int choice = 1;
-		while( choice == 1 )
-		{
-			this.readFromFile();
-
-		}
-
 	}
 }
