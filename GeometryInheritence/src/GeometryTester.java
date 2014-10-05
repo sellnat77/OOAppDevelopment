30d36b5663bae25b78c1526ef838ed46d0de0013
Russell <sell_nat@yahoo.com>
2014-10-05 13:20:44 -0700

    adding unstaged files

--- GeometryInheritence/src/GeometryTester.java
+++ GeometryInheritence/src/GeometryTester.java
@@ -9,71 +9,72 @@
 import java.util.ArrayList;
 public class GeometryTester
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		ArrayList<GeometricObject> myShapes = new ArrayList<GeometricObject>();
-		
+
 		//Instantiate 2 of each shape with different sizes
+		Object circle = new Circle();
 		GeometricObject Circle     = new Circle(5.0);
-		GeometricObject Circle2    = new Circle(10.0);		
+		GeometricObject Circle2    = new Circle(10.0);
 		GeometricObject Rectangle  = new Rectangle(3.0,6.0);
 		GeometricObject Rectangle2 = new Rectangle(5.0,6.0);
 		GeometricObject Triangle   = new Triangle(3.0,4.0,5.0);
 		GeometricObject Triangle2  = new Triangle(5.0,4.0,5.0);
-		
+
 		//Set the colors
 		Circle.setColor ("Blue");
 		Circle2.setColor("Red" );
-		
+
 		Rectangle.setColor ("Purple");
 		Rectangle2.setColor("Green" );
-		
-		Triangle.setColor ("Red" );		
+
+		Triangle.setColor ("Red" );
 		Triangle2.setColor("Cyan");
-		
+
 		//State whether filled or not
 		Circle.setFilled (false);
 		Circle2.setFilled(true );
-		
+
 		Rectangle.setFilled (true );
 		Rectangle2.setFilled(false);
-		
-		Triangle.setFilled (true );		
+
+		Triangle.setFilled (true );
 		Triangle2.setFilled(false);
-		
+
 		//Add them to an arraylist
 		myShapes.add(Circle);
 		myShapes.add(Circle2);
-		
+
 		myShapes.add(Rectangle);
 		myShapes.add(Rectangle2);
-		
-		myShapes.add(Triangle);		
+
+		myShapes.add(Triangle);
 		myShapes.add(Triangle2);
-		
+
 		//Print the stats for each
 		for(GeometricObject shapes:myShapes)
 		{
-			System.out.println(shapes.toString()+ "\n");			
+			System.out.println(shapes.toString()+ "\n");
 		}
-		
+
 		//Comparisons for each
 		System.out.print("Comparing the first circle with the second circle\n\t");
 		Circle.compareTo(Circle2);
 		System.out.print("\nComparing the second circle with the first circle\n\t");
 		Circle2.compareTo(Circle);
-		
+
 		System.out.print("\nComparing first rectangle with second rectangle\n\t");
 		Rectangle.compareTo(Rectangle2);
 		System.out.print("\nComparing second rectangle with first rectangle\n\t");
 		Rectangle2.compareTo(Rectangle);
-		
+
 		System.out.print("\nComparing the first triangle with the second one\n\t");
 		Triangle.compareTo(Triangle2);
 		System.out.print("\nComparing the second triangle with the first one\n\t");
 		Triangle2.compareTo(Triangle);
 
-		
+
 		//Are the two triangles equal?
 		System.out.print("\nDetermining if the two triangles are equal...\n\t");
 		Triangle.equals(Triangle2);
