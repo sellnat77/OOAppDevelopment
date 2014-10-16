1f067d2ff5cdc604d18fb3d99d807509cc0708aa
Russell <sell_nat@yahoo.com>
2014-10-16 13:02:49 -0700

    fixing formatting

--- RegexEC/RegexReader.java
+++ RegexEC/RegexReader.java
@@ -7,16 +7,17 @@ public class RegexReader
 {
 	private Scanner userInput = new Scanner(System.in);
 	private String text;
+	Pattern zz = Pattern.compile("zz");
+	Pattern xx = Pattern.compile("xx");
+	Pattern vow = Pattern.compile("[aeiou].+[aeiou].");
+	Pattern bigVow = Pattern.compile("[aeiou].+[aeiou].+[aeiou]");
+	Pattern qNoU = Pattern.compile("^q+!u");
 
 	RegexReader(){}
 
 	public void readScrabble()
 	{
-		Pattern zz = Pattern.compile("[z].+[z].");
-		Pattern xx = Pattern.compile("[xx]");
-		Pattern vow = Pattern.compile("[aeiou].+[aeiou].");
-		Pattern bigVow = Pattern.compile("[aeiou].+[aeiou].+[aeiou]");
-		Pattern qNoU = Pattern.compile("^q+!u");
+
 		try
 		{
 			JFileChooser fileC = new JFileChooser();
@@ -29,17 +30,12 @@ public class RegexReader
 				while(input.hasNext())
 				{
 					text = input.nextLine();
-					Matcher zzm = zz.matcher(text);
-					Matcher xxm = xx.matcher(text);
-					Matcher vowm = vow.matcher(text);
-					Matcher bigVowm = bigVow.matcher(text);
-					Matcher qNoUm = qNoU.matcher(text);
-
-					if(zzm.find())
+					if(this.checkZ(text)!= null)
 					{
-						System.out.println(text);
+						System.out.printf("%15s\t",this.checkZ(text));
 					}
 				}
+				System.out.println("");
 			}
 
 
@@ -55,4 +51,64 @@ public class RegexReader
 		}
 	}
 
+	public String checkZ(String text)
+	{
+		Matcher zzm = zz.matcher(text);
+		if(zzm.find())
+		{
+			return text;
+		}
+		else
+		{
+			return null;
+		}
+	}
+	public String checkX(String text)
+	{
+		Matcher xxm = xx.matcher(text);
+		if(xxm.find())
+		{
+			return text;
+		}
+		else
+		{
+			return null;
+		}
+	}
+	public String checkVow(String text)
+	{
+		Matcher vowm = vow.matcher(text);
+		if(vowm.find())
+		{
+			return text;
+		}
+		else
+		{
+			return null;
+		}
+	}
+	public String checkBigVow(String text)
+	{
+		Matcher bigVowm = bigVow.matcher(text);
+		if(bigVowm.find())
+		{
+			return text;
+		}
+		else
+		{
+			return null;
+		}
+	}
+	public String checkQNoU(String text)
+	{
+		Matcher qNoUm = qNoU.matcher(text);
+		if(qNoUm.find())
+		{
+			return text;
+		}
+		else
+		{
+			return null;
+		}
+	}
 }
