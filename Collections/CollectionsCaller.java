9ddd89221eb3ff81e51b8435bd734321a06d7eff
sellnat77 <sell_nat@yahoo.com>
2014-10-26 14:57:00 -0700

    Finished documentation

--- Collections/CollectionsCaller.java
+++ Collections/CollectionsCaller.java
@@ -1,12 +1,22 @@
+/*
+ * Name: Russell Tan
+ * Date: October 26, 2014
+ * Purpose: Determine pont values of scrabble words and analyze time difference between
+ *          Tree and hash set assignments
+ * Inputs: QNoU.txt points.txt
+ * Outputs: Point values of words and time taken by a large tree and hash set assignment
+ */
+
 public class CollectionsCaller
 {
 	public static void main(String args[])
 	{
 		CollectionsMaps coll = new CollectionsMaps();
 
+		//Read scrabble points and print point values of words in QNoU.txt
 		coll.readScabble();
 
+		//Adds large text file to tree and has map and print the time taken by each
 		coll.largeSets();
-
 	}
 }
